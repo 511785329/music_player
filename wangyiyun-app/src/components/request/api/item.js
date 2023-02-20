a41d23df5e2dd8e2ea44442a6cc9771c063ce763
VirgilRay <VirgilRay233@users.noreply.github.com>
2023-02-20 22:31:36 +0800

    feat(server): use china ip

--- wangyiyun-app/src/components/request/api/item.js
+++ wangyiyun-app/src/components/request/api/item.js
@@ -3,27 +3,27 @@ import service from "..";
 export function getMusicItemList(data) {
 	return service({
 		method: "GET",
-		url: `/playlist/detail?id=${data}`,
+		url: `/playlist/detail?id=${data}&realIP=116.25.146.177`,
 	});
 }
 // 获取歌单歌曲
 export function getItemList(data) {
 	return service({
 		method: "GET",
-		url: `/playlist/track/all?id=${data}&limit=20&offset=0`,
+		url: `/playlist/track/all?id=${data}&limit=20&offset=0&realIP=116.25.146.177`,
 	});
 }
 // 获取歌单歌曲url
 export function getMusicUrl(data) {
 	return service({
 		method: "GET",
-		url: `/song/url/v1?id=${data}&level=standard`,
+		url: `/song/url/v1?id=${data}&level=standard&realIP=116.25.146.177`,
 	});
 }
 // 获取歌词
 export function getMusicLyric(data) {
 	return service({
 		method: "GET",
-		url: `/lyric?id=${data}`,
+		url: `/lyric?id=${data}&realIP=116.25.146.177`,
 	});
 }
